42dab06b2cfc24021ca92c710c5bad6e8a342f36
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-07 19:34:07 +0300

    created catogory element

--- main.js
+++ main.js
@@ -20,3 +20,28 @@ document.addEventListener('DOMContentLoaded', () => {
         catCategoryRow.removeAttribute('hidden')
         catCategoryRow.style.display = "flex"
     })
+
+
+      // create category element
+      const createCategory = (image, breed, habitat, lifespan) => {
+        const rootDiv = document.createElement('div')
+        rootDiv.classList.add('col-4', 'p-1')
+
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('card', 'col-12', 'p-2')
+
+        const categoryImg = document.createElement('img')
+        categoryImg.classList.add('card-img-top')
+        categoryImg.src = image
+
+        const categoryTitle = document.createElement('h4')
+        categoryTitle.classList.add('card-title')
+        categoryTitle.innerText = breed
+
+        const categoryHabitat = document.createElement('p')
+        categoryHabitat.classList.add('card-text')
+        categoryHabitat.innerText = "Habitat: " + habitat
+
+        const categoryLifeSpan = document.createElement('p')
+        categoryLifeSpan.classList.add('card-text')
+        categoryLifeSpan.innerText = "Life Span: " + lifespan
