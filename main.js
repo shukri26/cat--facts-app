879bd98075c03bcbcca3c0874de37adacd8bfca9
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-07 19:36:47 +0300

    append title and image to card

--- main.js
+++ main.js
@@ -45,3 +45,38 @@ document.addEventListener('DOMContentLoaded', () => {
         const categoryLifeSpan = document.createElement('p')
         categoryLifeSpan.classList.add('card-text')
         categoryLifeSpan.innerText = "Life Span: " + lifespan
+
+            // append title and image to card
+            cardDiv.appendChild(categoryImg)
+            cardDiv.appendChild(categoryTitle)
+            cardDiv.appendChild(categoryHabitat)
+            cardDiv.appendChild(categoryLifeSpan)
+            rootDiv.appendChild(cardDiv)
+            return rootDiv
+        }
+        const loadCategories = () => {
+            fetch(CATEGORIES)
+                .then((response) => response.json())
+                .then((data) => {
+                    const categoriesData = data.cats
+                    const categoryElems = categoriesData.map(
+                        cats => createCategory(cats.image, cats.breed, cats.habitat, cats.lifespan)
+                    )
+                    catCategoryRow.append(...categoryElems)
+                })
+        }
+        loadCategories()
+    })
+    $(document).ready(function(){
+      $("#contact").click(function(){
+            $(".Home-Page").hide();
+            $("#cat-categories").hide();
+            $(".contact-page").show();
+      });
+      $("#home-link").click(function(){
+        $(".contact-page").hide();
+        $("#cat-categories").hide();
+        $(".Home-Page").show();
+      });
+    });
+    
