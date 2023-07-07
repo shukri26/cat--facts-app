638f6b5132d4304a98d61c75213a1ddb0766d6ae
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-07 19:30:20 +0300

    eventListerner for links

--- main.js
+++ main.js
@@ -5,8 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const catCategoryRow = document.getElementById('cat-categories')
     const hide = document.querySelector(".Home-Page")
     const contact = document.querySelector(".contact-page")
-    
+
     // LINKS DATA
     const categoriesLink = document.getElementById('category-link')
     const homeLink = document.getElementById('home-link')
     const contactSection = document.getElementById('contact')
+
+    // CLICK EVENTS FOR LINKS
+    categoriesLink.addEventListener('click', () => {
+        hide.style.display = "none"
+
+        contact.style.display = "none"
+        // show categories
+        catCategoryRow.removeAttribute('hidden')
+        catCategoryRow.style.display = "flex"
+    })
