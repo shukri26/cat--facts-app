3fd75f35cd10297d655aa28c2be33c712b585ac2
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-07 19:24:37 +0300

    created rows data

---
+++ main.js
@@ -0,0 +1,7 @@
+const CATEGORIES = 'https://al-hajjar.github.io/phase1project/db.json'
+document.addEventListener('DOMContentLoaded', () => {
+
+    // ROWS DATA
+    const catCategoryRow = document.getElementById('cat-categories')
+    const hide = document.querySelector(".Home-Page")
+    const contact = document.querySelector(".contact-page")
